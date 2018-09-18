81d171bebd81070ef148b3ff9816636a0d29c82c
Carlos <chs.ponciano@gmail.com>
2018-09-18 19:50:11 -0300

    alterações

--- Trabalho4/Banco.cs
+++ Trabalho4/Banco.cs
@@ -8,6 +8,42 @@ namespace Trabalho4
 {
     class Banco
     {
+        private List<ContaBase> contas { get; }
 
+        public int addConta(String nome, DateTime dataNascimento)
+        {
+            return 1;
+        }
+
+        public int addConta(String nome, DateTime dataNascimento, Mes mesConta)
+        {
+            return 1;
+        }
+
+        public String listarContas()
+        {
+            return null;
+        }
+
+        public bool saque(int numeroConta, float valorSaque, out float taxa)
+        {
+            taxa = 0;
+            return true;
+        }
+
+        public bool deposito(int numeroConta, float valor)
+        {
+            return true;
+        }
+
+        public float totalConta(int numeroConta)
+        {
+            return 5f;
+        }
+
+        public float emprestimo(int numeroConta, float valor)
+        {
+            return 5f;
+        }
     }
 }
