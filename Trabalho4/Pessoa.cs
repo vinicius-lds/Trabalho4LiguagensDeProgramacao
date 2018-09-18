255cc63532a6184cfe8799a4fd2085a2f326d65e
Carlos <chs.ponciano@gmail.com>
2018-09-18 19:17:45 -0300

    arquivos

---
+++ Trabalho4/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho4
+{
+    class Pessoa
+    {
+        private String nome { get; }
+        private DateTime dataNascimento { get; }
+
+        public Pessoa(String nome, DateTime dataNascimento)
+        {
+            this.dataNascimento = dataNascimento;
+            this.nome = nome;
+        }
+    }
+}
