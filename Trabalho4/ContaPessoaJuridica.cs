2d20bd0bfd2094874543b43454963f59bd312028
vinicius <vinicius.lds.br@gmail.com>
2018-09-18 19:55:24 -0300

    qualquer merda

---
+++ Trabalho4/ContaPessoaJuridica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho4
+{
+    class ContaPessoaJuridica : ContaBase, Transacao
+    {
+
+        public ContaPessoaJuridica(string nome, DateTime dataDeNascimento) : base(nome, dataDeNascimento)
+        {
+            
+        }
+
+        public bool depositar(float valorDoDeposito)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float empresta(float valorEmprestimo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool sacar(float valorDoSaque, out float taxaDoSaque)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Trabalho4/Transacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho4
+{
+    interface Transacao
+    {
+        bool sacar(float valorDoSaque, out float taxaDoSaque);
+        bool depositar(float valorDoDeposito);
+        float empresta(float valorEmprestimo);
+    }
+}
